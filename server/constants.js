cf840b3cc38c3058ee20e7c27164d4e83b4f2841
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-08-01 18:48:18 +0530

    feat: add registered user field in events

--- server/constants.js
+++ server/constants.js
@@ -7,6 +7,7 @@ module.exports = {
       description: 'Vinyasa means "to place in a special way" and, in this case, yoga postures.',
       timestamp: 1596285000000,
       timestring: '1st August - 6 PM',
+      registeredUsers: [],
     },
     {
       name: 'Restorative yoga',
@@ -15,6 +16,7 @@ module.exports = {
       description: 'Restorative yoga focuses on winding down after a long day and relaxing your mind.',
       timestamp: 1596288600000,
       timestring: '1st August - 7 PM',
+      registeredUsers: [],
     },
     {
       name: 'Hatha yoga yoga',
@@ -23,6 +25,7 @@ module.exports = {
       description: 'The Sanskrit term "hatha" is an umbrella term for all physical postures of yoga.',
       timestamp: 1596292200000,
       timestring: '1st August - 8 PM',
+      registeredUsers: [],
     },
     {
       name: 'Restorative yoga',
@@ -31,6 +34,7 @@ module.exports = {
       description: 'Restorative yoga focuses on winding down after a long day and relaxing your mind.',
       timestamp: 1596375000000,
       timestring: '2nd August - 7 PM',
+      registeredUsers: [],
     },
     {
       name: 'Yin yoga',
@@ -40,6 +44,7 @@ module.exports = {
         'Yin yoga is a slow-paced style of yoga with seated postures that are held for longer periods of time.',
       timestamp: 1596375000000,
       timestring: '2nd August - 7 PM',
+      registeredUsers: [],
     },
     {
       name: 'Yin yoga',
@@ -49,6 +54,7 @@ module.exports = {
         'Yin yoga is a slow-paced style of yoga with seated postures that are held for longer periods of time.',
       timestamp: 1596634200000,
       timestring: '5th August - 7 PM',
+      registeredUsers: [],
     },
     {
       name: 'Anusara yoga',
@@ -58,6 +64,7 @@ module.exports = {
         'Anusara is a modern-day version of hatha yoga, most similar to vinyasa in that it focuses on alignment, but with more focus on the mind-body-heart connection.',
       timestamp: 1597458600000,
       timestring: '15th August - 8 AM',
+      registeredUsers: [],
     },
   ],
 };
