e7e0e1d9d085f4cb54fab4d3dddeb5783c731291
Abhishek Khanna <abhishekkhanna1995@gmail.com>
2020-08-01 18:48:44 +0530

    feat: update server implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const lowdb = require('lowdb');
+const morgan = require('morgan');
+const FileSync = require('lowdb/adapters/FileSync');
 
+const adapter = new FileSync('db.json');
+const db = lowdb(adapter);
 const { events } = require('./constants');
+
+morgan(':method :url :status :res[content-length] - :response-time ms');
+db.defaults({ events }).write();
 
 const app = express();
 
@@ -16,6 +24,7 @@ app.get('/events', (req, res) => {
 
     if (!date) return res.send(events);
 
+    const events = db.get('events').value();
     const filteredEvents = events.filter(({ timestamp }) => {
       const eventDate = new Date(timestamp).toDateString();
       return new Date(parseInt(date, 10)).toString().includes(eventDate);
@@ -24,7 +33,7 @@ app.get('/events', (req, res) => {
     return res.send(filteredEvents);
   } catch (error) {
     console.log(error);
-    res.send(500);
+    return res.sendStatus(500);
   }
 });
 
@@ -32,47 +41,66 @@ app.get('/events/occurences', (req, res) => {
   try {
     const currentTimestamp = new Date().getTime();
 
+    const events = db.get('events').value();
     const filteredEvents = events
-      .filter(({ timestamp }) => {
-        console.log(timestamp > currentTimestamp);
-        return timestamp > currentTimestamp;
-      })
+      .filter(({ timestamp }) => timestamp > currentTimestamp)
       .map(({ timestamp }) => timestamp);
 
     return res.send(filteredEvents);
   } catch (error) {
     console.log(error);
-    res.send(500);
+    return res.sendStatus(500);
   }
 });
 
 // @TODO: do permission checks
 
-app.get('/events/register', (req, res) => {
+app.post('/events/register', (req, res) => {
   try {
-    const { userId, eventId, username, email } = req.body;
+    const { userId, eventId, name, email } = req.body;
 
-    if (!userId || !eventId || !username || !email) {
+    if (!userId || !eventId || !name || !email) {
       throw { reason: 'Missing / invalid arguments', status: 400 };
     }
+
+    const event = db.get('events').find({ id: eventId }).value();
+    const existingUsers = event.registeredUsers;
+
+    if (existingUsers.some(({ userId: id }) => id === userId)) {
+      return res.sendStatus(200);
+    }
+
+    existingUsers.push({ userId, name, email });
+
+    db.get('events').find({ id: eventId }).update({ registeredUsers: existingUsers }).write();
+
+    return res.sendStatus(200);
   } catch (error) {
+    console.log(error);
     if (error.status) {
       return res.status(error.status).send(error);
     }
 
-    return res.send(500);
+    return res.sendStatus(500);
   }
 });
 
 app.get('/users/:userId/registered-events', (req, res) => {
   try {
-    // const userId = req.params.userId;
+    const userId = parseInt(req.params.userId);
+    const events = db.get('events').value();
 
-    res.send([]);
+    const filteredEvents = events.filter(({ registeredUsers }) =>
+      registeredUsers.some(({ userId: id }) => id === userId)
+    );
+
+    return res.send(filteredEvents);
   } catch (error) {
     console.log(error);
-    res.send(500);
+    return res.sendStatus(500);
   }
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log('listening...');
+});
